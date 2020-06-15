2296f9de4a3a6b73d0ad0e596be61613de70a576
edutamura <51760396+edutamura@users.noreply.github.com>
2020-06-15 17:46:23 -0300

    teste banco

--- banco/conexao.php
+++ banco/conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-define('HOST', 'us-cdbr-east-06.cleardb.net');
+define('HOST', 'us-cdbr-east-06.cleardb.net:3306');
 define('USUARIO','bd633b219fc125');
 define('SENHA', '9efd6f04');
 define('DB','heroku_9c64e6b5f3a5cf6');
