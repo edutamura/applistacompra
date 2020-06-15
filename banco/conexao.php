8e16417323d5fd577a57fb2f79345c18dcfd76a1
edutamura <51760396+edutamura@users.noreply.github.com>
2020-06-15 17:40:22 -0300

    Adicionando configurações de banco

--- banco/conexao.php
+++ banco/conexao.php
@@ -1,9 +1,9 @@
 <?php
 
-define('HOST', 'localhost');
-define('USUARIO','listacompras');
-define('SENHA', 'lista1234#');
-define('DB','listacompras');
+define('HOST', 'us-cdbr-east-06.cleardb.net');
+define('USUARIO','bd633b219fc125');
+define('SENHA', '9efd6f04');
+define('DB','heroku_9c64e6b5f3a5cf6');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die('Erro ao conectar');
 
